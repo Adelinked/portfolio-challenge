9bb76dcade1e022b93cacd454cc7325b945d58d8
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2023-09-14 07:55:23 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,21 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        gray1: "#333",
+        gray2: "#4F4F4F",
+        gray3: "#828282",
+        gray4: "#C4C4C4",
+        blue1: "#2F80ED"
+      },
+
     },
   },
   plugins: [],
